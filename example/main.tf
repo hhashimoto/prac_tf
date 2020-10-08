186d4b90ff8d1a61b50874d6b5da0306c0cf4bd9
hhashimoto <almite.bridge@gmail.com>
2020-10-08 20:26:52 +0900

    5.2.4 IAMロールのモジュール化

--- example/main.tf
+++ example/main.tf
@@ -19,28 +19,9 @@ data "aws_iam_policy_document" "allow_describe_regions" {
   }
 }
 
-resource "aws_iam_policy" "example" {
-  name   = "example"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
-}
-
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name               = "example"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "example" {
-  role       = aws_iam_role.example.name
-  policy_arn = aws_iam_policy.example.arn
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
