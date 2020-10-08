785ebe35812baf5efc0ac4355dc533614ee80548
hhashimoto <almite.bridge@gmail.com>
2020-10-08 20:03:31 +0900

    5.1 IAMポリシー
    IAMポリシードキュメント(UI上ではJSON形式)の定義と、IAMポリシーの定義

--- example/main.tf
+++ example/main.tf
@@ -10,3 +10,16 @@ module "web_server" {
 output "public_dns" {
   value = module.web_server.public_dns
 }
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"] # リージョン一覧の取得
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "example" {
+  name   = "example"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
