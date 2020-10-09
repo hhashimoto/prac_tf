5edbc58c0fba5bcaafe445891d5d5a9a92260c27
hhashimoto <almite.bridge@gmail.com>
2020-10-09 12:07:32 +0900

    6.3 ログバケット
    lifecycle_ruleでログローテーションを実現、
    バケットポリシーでALBの東京リージョンからのログ書き込みを許可している。
    
    ALB各リージョンのAWSアカウントIDについては、こちらを参照のこと。
    https://docs.aws.amazon.com/ja_jp/elasticloadbalancing/latest/classic/enable-access-logs.html

--- example/main.tf
+++ example/main.tf
@@ -61,3 +61,33 @@ resource "aws_s3_bucket" "public" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "h2-alb-log-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
